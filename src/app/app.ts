0c871850a2ab45dbb7cf4cab9a750a8ec99de499
Javier Flores <jffc.dev@gmail.com>
2026-08-05 19:03:24 -0500

    agregando buscador al reto 2 (html y estilos)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Reto01TodoStats } from './ejercicios/reto-01-todo-stats/reto-01-todo-stats';
+import { Reto02Carrito } from './ejercicios/reto-02-carrito/reto-02-carrito';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Reto01TodoStats, Reto02Carrito],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('retos-angular-semana3');
-}
+export class App {}
