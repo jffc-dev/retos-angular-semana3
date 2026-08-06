a2cde6325b6f4fd470efca0fac752cef12004944
Javier Flores <jffc.dev@gmail.com>
2026-08-05 21:07:37 -0500

    agregando reto de inputs

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Reto01TodoStats } from './ejercicios/reto-01-todo-stats/reto-01-todo-stats';
 import { Reto02Carrito } from './ejercicios/reto-02-carrito/reto-02-carrito';
+import { Reto03Inputs } from './ejercicios/reto-03-inputs/reto-03-inputs';
 
 @Component({
   selector: 'app-root',
-  imports: [Reto01TodoStats, Reto02Carrito],
+  imports: [Reto01TodoStats, Reto02Carrito, Reto03Inputs],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
---
+++ src/app/ejercicios/reto-03-inputs/reto-03-inputs.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reto-03-inputs',
+  standalone: true,
+  imports: [],
+  templateUrl: './reto-03-inputs.html',
+})
+export class Reto03Inputs {
+
+}
