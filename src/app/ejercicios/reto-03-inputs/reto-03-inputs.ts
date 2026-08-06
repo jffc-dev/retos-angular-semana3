113cb44ece6028c6925750e49957b626e7e83a3e
Javier Flores <jffc.dev@gmail.com>
2026-08-05 21:32:31 -0500

    agregando logica para manejar inputs

--- src/app/ejercicios/reto-03-inputs/reto-03-inputs.ts
+++ src/app/ejercicios/reto-03-inputs/reto-03-inputs.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, model, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reto-03-inputs',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './reto-03-inputs.html',
 })
 export class Reto03Inputs {
 
+  inputModel = model('')
+
+  inputSignal = signal('')
+
+  actualizarInputSignal(event: Event){
+    this.inputSignal.set((event.target as HTMLInputElement).value)
+  }
 }
