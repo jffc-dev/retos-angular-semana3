d51ae2409c8624187881ba6f203b58fce0cc1f58
Javier Flores <jffc.dev@gmail.com>
2026-08-07 22:19:59 -0500

    agregar logica del validador de contrasenias

--- src/app/ejercicios/reto-05-password-strength/reto-05-password-strength.ts
+++ src/app/ejercicios/reto-05-password-strength/reto-05-password-strength.ts
@@ -1,3 +1,4 @@
+import { NgStyle, NgClass } from '@angular/common';
 import { Component, computed, signal } from '@angular/core';
 
 type NivelFuerza = 'vacio' | 'debil' | 'media' | 'fuerte';
@@ -5,8 +6,9 @@ type NivelFuerza = 'vacio' | 'debil' | 'media' | 'fuerte';
 @Component({
   selector: 'app-reto-05-password-strength',
   standalone: true,
-  imports: [],
+  imports: [NgStyle, NgClass],
   templateUrl: './reto-05-password-strength.html',
+  styleUrl: './reto-05-password-strength.css'
 })
 export class Reto05PasswordStrength {
   // TODO: importa NgStyle y NgClass de '@angular/common' y agrégalos a
@@ -40,4 +42,27 @@ export class Reto05PasswordStrength {
   actualizarPassword(event: Event): void {
     this.password.set((event.target as HTMLInputElement).value);
   }
+
+  porcentaje = computed<number>(()=>{
+    const valores = { vacio:0, debil:33, media:66, fuerte:100 }
+    return valores[this.nivel()]
+  })
+
+  color = computed<string>(() => {
+    const valores = { vacio:'white', debil:'red', media:'#B2B83E', fuerte:'green' }
+    return valores[this.nivel()]
+  })
+
+  colorTexto = computed<object>(()=>{
+    return {
+      'text-gray-400': this.nivel() === 'vacio',
+      'texto-password-debil': this.nivel() === 'debil',
+      'text-yellow-500': this.nivel() === 'media',
+      'text-green-500': this.nivel() === 'fuerte'
+    }
+  })
+
+  estilosBarra = computed<object>(()=> {
+    return {width: this.porcentaje() + '%', 'background-color': this.color()}
+  })
 }
